0d8b1873276e5979792bd064df5ae1e1482358d3
Thomas Sänger <HorayNarea@gmail.com>
2016-06-12 13:39:12 +0200

    add release script

---
+++ release.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# force static build
+export CGO_ENABLED=0
+
+compile() {
+	echo "Compiling for $1 on $2..."
+	GOOS=$1 GOARCH=$2 GOARM=$3 go build -o bin/SoundBert-$1-$2 .
+}
+
+for OS in freebsd linux windows; do
+	for ARCH in amd64 386; do
+		compile $OS $ARCH
+	done
+done
+
+# Mac OS X doesn't need 32-bit
+compile darwin amd64
+
+# build for Raspberry-Pi
+compile linux arm 6
